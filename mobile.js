363a8946653e788b314a98a07653245e4722c9c6
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-04-19 21:46:36 +0100

    Delete mobile.js

--- mobile.js
+++
@@ -1,8 +0,0 @@
-if (innerWidth < 900) {
-    document.body.innerHTML = "";
-
-    var unavailable = document.createElement("div");
-    unavailable.classList.add("unavailable");
-    unavailable.innerHTML = "Sorry, GameZone isn't Available for Mobile";
-    document.body.appendChild(unavailable);
-}
