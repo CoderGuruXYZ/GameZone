61a7d852079bf29f4dc90e7c89ea57376ad5e60a
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-04-19 21:51:15 +0100

    Delete fixsize.js

--- fixsize.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("iframe").style.border = "0";
-document.querySelector("iframe").style.width = "100%";
-document.querySelector("iframe").style.height = "100%";
-document.querySelector("iframe").style.borderRadius = "15px";
