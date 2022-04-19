6b047238905785b0d701eb679ad87818a8352fb4
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-04-19 22:01:27 +0100

    Fix Link Slash

--- scripts/search.js
+++ scripts/search.js
@@ -1,5 +1,5 @@
 document.querySelector(".searchInput").addEventListener("change", function () {
-    var searchLink = window.origin + 'search.html?query=' + document.querySelector(".searchInput").value;
+    var searchLink = window.origin + '/search.html?query=' + document.querySelector(".searchInput").value;
 
     window.location.href = searchLink;
 });
