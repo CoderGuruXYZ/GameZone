5953100ba72588f2f53e1e227ef147569af20258
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-04-19 22:00:49 +0100

    Fix Redirect

--- scripts/search.js
+++ scripts/search.js
@@ -1,5 +1,5 @@
 document.querySelector(".searchInput").addEventListener("change", function () {
-    window.location.href = "search.html/"
-
     var searchLink = window.origin + 'search.html?query=' + document.querySelector(".searchInput").value;
+
+    window.location.href = searchLink;
 });
